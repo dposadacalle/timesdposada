fc8b6aa749aae6b684899c75bc6ccde49509b615
Daniel Posada Calle <posadacalled@gmail.com>
2021-09-06 21:42:13 -0500

    Archicture project

---
+++ timesdposada/timesdposada.Common/Models/Consolidated.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace timesdposada.Common.Models
+{
+    public class Consolidated
+    {
+        public int EmployeeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int MinutesCompleted { get; set; }
+    }
+}
---
+++ timesdposada/timesdposada.Common/Models/Time.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace timesdposada.Common.Models
+{
+    public class Time
+    {
+        public int EmployeeId { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public bool Consolidate { get; set; }
+    }
+}
